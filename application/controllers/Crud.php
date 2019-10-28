6b6394af739b41313e1bb422a659892342909650
assenadamas <assenadamas7@gmail.com>
2019-10-28 15:23:38 +0700

    Update data di Database

--- application/controllers/Crud.php
+++ application/controllers/Crud.php
@@ -41,4 +41,34 @@ class Crud extends CI_Controller{
 		redirect('crud/index');
 	}
 
+	function edit($id){
+		$where = array('id' => $id);
+		$data['data_mahasiswa'] = $this->m_data->edit_data($where,'data_mahasiswa')->result();
+		$this->load->view('v_edit',$data);
+	}
+
+	function update(){
+	$id = $this->input->post('id');
+	$nim = $this->input->post('nim');
+	$nama = $this->input->post('nama');
+	$jenis_kelamin = $this->input->post('jenis_kelamin');
+	$hp = $this->input->post('hp');
+	$alamat = $this->input->post('alamat');
+ 
+	$data = array(
+		'nim' => $nim,
+		'nama' => $nama,
+		'jenis_kelamin' => $jenis_kelamin,
+		'hp' => $hp,
+		'alamat' => $alamat
+	);
+ 
+	$where = array(
+		'id' => $id
+	);
+ 
+	$this->m_data->update_data($where,$data,'data_mahasiswa');
+	redirect('crud/index');
+	}
+
 }
